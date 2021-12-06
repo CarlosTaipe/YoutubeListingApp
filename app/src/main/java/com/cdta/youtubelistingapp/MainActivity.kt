4bbe810302dffd4bf230214dd518df80cd9ec722
Carlos Taipe <carlostaipe9025@gmail.com>
2021-12-06 11:02:18 -0300

    load CategoryList in View!

--- app/src/main/java/com/cdta/youtubelistingapp/MainActivity.kt
+++ app/src/main/java/com/cdta/youtubelistingapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.cdta.youtubelistingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.cdta.youtubelistingapp.model.Category
 import com.parse.ParseObject
 import com.parse.ParseQuery
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "BrowserCategories:"
+    var categoryList = ArrayList<Category>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,26 +21,37 @@ class MainActivity : AppCompatActivity() {
         loadCategories()
     }
 
-    private fun loadCategories(){
+    private fun loadCategories() {
         val query = ParseQuery<ParseObject>("Category")
         query.orderByAscending("name")
-        query.findInBackground{list, e->
-            if (e==null){
+        query.findInBackground { list, e ->
+            if (e == null) {
                 //No error occured
-                Log.d(TAG,"$TAG: No error ocurred when running the query")
-                if (list.size>0){
+                Log.d(TAG, "$TAG: No error ocurred when running the query")
+                if (list.size > 0) {
                     //there is categories retrived
-                    Log.d(TAG,"$TAG: There is "+ list.size + "categories retrieved")
-                    for (category in list){
-                        Log.d(TAG,"$TAG: "+ category.get("name"))
+                    Log.d(TAG, "$TAG: There is " + list.size + "categories retrieved")
+                    for (category in list) {
+                        categoryList.add(
+                            Category(
+                                category.objectId, category.getString("name").toString(),
+                                category.getParseFile("picture")
+                            )
+                        )
+                        Log.d(TAG, "$TAG: categoryList content:  " + categoryList.toString())
+                        var nameOfCategories = ""
+                        for (c in categoryList){
+                            nameOfCategories+=c.name+"\n"
+                        }
+                        category_list.text = nameOfCategories
                     }
-                }else{
+                } else {
                     //there is no categories in the app backend
-                    Log.d(TAG,"$TAG: There is no categories in the app backed")
+                    Log.d(TAG, "$TAG: There is no categories in the app backed")
                 }
-            }else{
+            } else {
                 //there is error occured
-                Log.d(TAG,"$TAG: There is an error occured "+e.message)
+                Log.d(TAG, "$TAG: There is an error occured " + e.message)
             }
         }
     }
