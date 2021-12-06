1d1e8d4f57d175d3cad33baebc1e028225ad36b1
Carlos Taipe <carlostaipe9025@gmail.com>
2021-12-06 10:46:06 -0300

    first load categories!

--- app/src/main/java/com/cdta/youtubelistingapp/MainActivity.kt
+++ app/src/main/java/com/cdta/youtubelistingapp/MainActivity.kt
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.parse.ParseObject
+import com.parse.ParseQuery
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "BrowserCategories:"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val firstObject = ParseObject("FirstClass")
-        firstObject.put("message","Hey ! First message from android. Parse is now connected")
-        firstObject.saveInBackground {
-            if (it != null){
-                it.localizedMessage?.let { message -> Log.e("MainActivity", message) }
+
+        loadCategories()
+    }
+
+    private fun loadCategories(){
+        val query = ParseQuery<ParseObject>("Category")
+        query.findInBackground{list, e->
+            if (e==null){
+                //No error occured
+                Log.d(TAG,"$TAG: No error ocurred when running the query")
+                if (list.size>0){
+                    //there is categories retrived
+                    Log.d(TAG,"$TAG: There is "+ list.size + "categories retrieved")
+                    for (category in list){
+                        Log.d(TAG,"$TAG: "+ category.get("name"))
+                    }
+                }else{
+                    //there is no categories in the app backend
+                    Log.d(TAG,"$TAG: There is no categories in the app backed")
+                }
             }else{
-                Log.d("MainActivity","Object saved.")
+                //there is error occured
+                Log.d(TAG,"$TAG: There is an error occured "+e.message)
             }
         }
     }
