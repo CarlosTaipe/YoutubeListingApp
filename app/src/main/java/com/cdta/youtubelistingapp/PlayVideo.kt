c43deec511dc4a5f462a6fec9c5d29fc95edc17c
Carlos Taipe <carlostaipe9025@gmail.com>
2021-12-07 17:29:13 -0300

    playingVideo in fullScreen!

--- app/src/main/java/com/cdta/youtubelistingapp/PlayVideo.kt
+++ app/src/main/java/com/cdta/youtubelistingapp/PlayVideo.kt
@@ -1,13 +1,15 @@
 package com.cdta.youtubelistingapp
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_play_video.*
 
 class PlayVideo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_play_video)
 
 
