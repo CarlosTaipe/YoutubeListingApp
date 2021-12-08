87d2000b5870a622f12045e42227c7cf3c5b850e
Carlos Taipe <carlostaipe9025@gmail.com>
2021-12-07 23:37:21 -0300

    playingVideo Finished!

--- app/src/main/java/com/cdta/youtubelistingapp/PlayVideo.kt
+++ app/src/main/java/com/cdta/youtubelistingapp/PlayVideo.kt
@@ -1,28 +1,39 @@
 package com.cdta.youtubelistingapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_play_video.*
 
 class PlayVideo : AppCompatActivity() {
+
+    val TAG = "PlayVideo:"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN)
         supportActionBar?.hide()
         setContentView(R.layout.activity_play_video)
 
-
         val videoYoutubeId = intent.getStringExtra("youtubeId")
-        var videoURL = "https://www.youtube.com/embed/"+videoYoutubeId
+        Log.d(TAG,"$TAG: Selected video id " + videoYoutubeId)
+        var videoURL = "https://www.youtube.com/embed/" + videoYoutubeId
 
-//        Toast.makeText(
-//            this,
-//            "Selected video id " + videoYoutubeId,
-//            Toast.LENGTH_LONG
-//        ).show()
-
-        my_web_view.settings.javaScriptEnabled=true
+        my_web_view.settings.javaScriptEnabled = true
+        my_web_view.webChromeClient = object : WebChromeClient(){
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                loading_play_video_message.text =
+                    getString(R.string.loading_youtube_video_message) + " " + newProgress + " %"
+                if (newProgress == 100) {
+                    progress_bar.visibility = View.GONE
+                    my_web_view.visibility = View.VISIBLE
+                }
+            }
+        }
         my_web_view.loadUrl(videoURL)
     }
 }
